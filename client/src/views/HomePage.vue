3cc6f4c465efdbc1ebad471547d52d2832174596
Chaewon Jeong <fromnowwon@gmail.com>
2025-06-12 19:20:00 +0900

    feat: 각 페이지 생성

---
+++ client/src/views/HomePage.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="min-h-screen flex items-center justify-center bg-gray-50">
+    <p>안녕하세요! ㅇㅇㅇ님</p>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
---
+++ client/src/views/ProfilePage.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>프로필 페이지</div>
+</template>
+
+<script></script>
+
+<style lang="scss" scoped></style>
