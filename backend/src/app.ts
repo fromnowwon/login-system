e5adde31f5884a4130ea9b3b96285a0dcbb42f0b
Chaewon Jeong <fromnowwon@gmail.com>
2025-06-14 23:08:48 +0900

    chore: cors 허용 및 프록시 서버 설정

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,11 +1,13 @@
 // 서버 설정 및 라우팅
 import express from "express";
 import dotenv from "dotenv";
+import cors from "cors";
 import authRoutes from "./routes/auth.routes";
 
 dotenv.config();
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use("/api/auth", authRoutes);
 
