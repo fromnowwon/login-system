fe507af028ec7d8a61dcd933ae3dc40057d7df27
Chaewon Jeong <fromnowwon@gmail.com>
2025-06-17 16:27:50 +0900

    chore: host 설정

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,8 +1,9 @@
 // 서버 실행
 import app from "./app";
 
-const PORT = process.env.PORT || 4000;
+const PORT = Number(process.env.PORT) || 4000;
+const HOST = "0.0.0.0";
 
-app.listen(PORT, () => {
-  console.log(`서버가 http://localhost:${PORT} 에서 실행 중입니다.`);
+app.listen(PORT, HOST, () => {
+  console.log(`서버가 http://${HOST}:${PORT} 에서 실행 중입니다.`);
 });
