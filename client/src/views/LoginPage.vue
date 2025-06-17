602d09106ca3cc8768f0c4316dfc10e346112e34
Chaewon Jeong <fromnowwon@gmail.com>
2025-06-17 21:51:18 +0900

    feat: 로그인 성공 후 로직 추가

--- client/src/views/LoginPage.vue
+++ client/src/views/LoginPage.vue
@@ -6,4 +6,32 @@
 
 <script setup lang="ts">
 import LoginForm from "@/components/LoginForm.vue";
+import { onMounted, onBeforeUnmount } from "vue";
+import { useAuthStore } from "@/stores/auth";
+
+const authStore = useAuthStore();
+
+function handleMessage(event: MessageEvent) {
+  console.log("Message origin:", event.origin);
+
+  if (event.origin !== import.meta.env.VITE_CLIENT_URL) return;
+
+  if (event.data.type === "google-login-success") {
+    const token = event.data.token;
+    authStore.token = token;
+    localStorage.setItem("token", token);
+    authStore.verifyCertificate();
+
+    // 팝업 닫히고 나서 로그인 성공 후 처리
+    window.location.href = "/";
+  }
+}
+
+onMounted(() => {
+  window.addEventListener("message", handleMessage);
+});
+
+onBeforeUnmount(() => {
+  window.removeEventListener("message", handleMessage);
+});
 </script>
